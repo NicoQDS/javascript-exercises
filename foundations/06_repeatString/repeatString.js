dd7491b5a396f437cd00ebf225a3abe1be4af4ee
NicoQDS <nico.osnaghi@gmail.com>
2026-05-10 22:23:04 +0200

    Solved the excercise repeat string.

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return "ERROR"
+    }
 
+    let stringToRepeat = ""
+    for (let i=0; i<num; i++) {
+        stringToRepeat += string
+    }
+    return stringToRepeat
 };
 
 // Do not edit below this line
